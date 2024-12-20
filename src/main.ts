4e446f0bedc1a3dc9a2c165f204442e5b6688e6b
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-20 16:02:30 +0330

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentVariables } from './common/env.validation';
+import { GlobalValidationPipe } from './common/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Get env config
   const configService: ConfigService<EnvironmentVariables> = app.get(ConfigService);
 
+  // Add global validation pipe
+  app.useGlobalPipes(GlobalValidationPipe);
 
-  await app.listen(configService.get("BACKEND_PORT"), "0.0.0.0", async () => {
+  // Start the app
+  await app.listen(configService.get('BACKEND_PORT'), '0.0.0.0', async () => {
     console.log(`server is running on: ${await app.getUrl()}`);
   });
 }
