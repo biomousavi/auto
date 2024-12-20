41d590e2959104aed6f01a5bab8c612303c9e55a
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-20 16:34:59 +0330

    feat: add pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ThrottlerModule } from "@nestjs/throttler";
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppService } from './app.service';
 import { PersistenceModule } from './persistence/persistence.module';
-import { validate } from './common/env.validation';
+import { EnvironmentVariables, validate } from './common/env.validation';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ validate, isGlobal: true }),
     ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+        type: 'postgres',
+        host: 'postgres',
+        port: +configService.get('PG_PORT'),
+        username: configService.get('PG_USERNAME'),
+        password: configService.get('PG_PASSWORD'),
+        database: configService.get('PG_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') === 'production' ? false : true,
+      }),
+    }),
     PersistenceModule,
   ],
   controllers: [AppController],
