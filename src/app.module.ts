c824883af41afb2a4caf319e19a1d39fb6a98e87
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-20 16:10:05 +0330

    feat: add throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ThrottlerModule } from "@nestjs/throttler";
 import { AppService } from './app.service';
 import { PersistenceModule } from './persistence/persistence.module';
 import { validate } from './common/env.validation';
@@ -8,6 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ validate, isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),
     PersistenceModule,
   ],
   controllers: [AppController],
