6815d4755874dd267518901dea74e8ff124a6868
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-20 15:59:00 +0330

    chore: use type for port env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from './common/env.validation';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService: ConfigService<EnvironmentVariables> = app.get(ConfigService);
+
+
+  await app.listen(configService.get("BACKEND_PORT"), "0.0.0.0", async () => {
+    console.log(`server is running on: ${await app.getUrl()}`);
+  });
 }
 bootstrap();
