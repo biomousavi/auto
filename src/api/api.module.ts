6c2d217d7ec23db08e480c7865c555a86c324409
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-21 17:57:38 +0330

    feat: add providers

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { InfrastructureModule } from 'src/infrastructure/infrastructure.module';
+import { UserController } from './controller/user.controller';
+import { CityController } from './controller/city.controller';
+import { GetProfileHandler } from './queries/handlers/get-profile.handler';
+import { GetCityHandler } from './queries/handlers/get-city.handler';
+import { GetMyRequestsHandler } from './queries/handlers/get-requests.handler';
+import { QueryHandlers } from './queries/handlers';
 
-@Module({})
+@Module({
+  imports: [CqrsModule, InfrastructureModule],
+  controllers: [UserController, CityController],
+  providers: [...QueryHandlers, GetProfileHandler, GetCityHandler, GetMyRequestsHandler],
+})
 export class ApiModule {}
