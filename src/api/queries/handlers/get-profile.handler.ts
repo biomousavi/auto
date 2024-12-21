87b87a13812d29acddae5e3cfbbef498e7cf6869
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-21 17:55:55 +0330

    feat: add get user profile handler

---
+++ src/api/queries/handlers/get-profile.handler.ts
@@ -0,0 +1,13 @@
+import { QueryHandler, IQueryHandler } from '@nestjs/cqrs';
+import { GetProfileQuery } from '../get-profile.query';
+import { UserService } from 'src/infrastructure/services/user.service';
+
+@QueryHandler(GetProfileQuery)
+export class GetProfileHandler implements IQueryHandler<GetProfileQuery> {
+  constructor(private readonly userService: UserService) {}
+
+  async execute(query: GetProfileQuery) {
+    console.log(query, 'executed');
+    return 'ok';
+  }
+}
