6d542f9082d335d8b38cea79b9f639b1fe2435e9
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-21 15:11:08 +0330

    feat: city service methods

---
+++ src/infrastructure/services/city.service.ts
@@ -0,0 +1,14 @@
+// infrastructure/services/city.service.ts
+import { Injectable } from '@nestjs/common';
+import { CityRepository } from '../../persistence/repositories/city.repository';
+import { ZipCodeService } from './zip-code.service';
+
+@Injectable()
+export class CityService {
+  constructor(
+    private readonly cityRepository: CityRepository,
+    private readonly zipCodeService: ZipCodeService,
+  ) {}
+
+  async getCityByZipCode(zipCode: string) {}
+}
