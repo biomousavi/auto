e243b802225f380b83550a47123e577057902248
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-21 15:14:13 +0330

    feat: add env type

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -7,17 +7,18 @@ import { ZipCodeService } from './services/zip-code.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EncryptionService } from './services/encryption.service';
+import { EnvironmentVariables } from 'src/common/env.validation';
 
 @Module({
   imports: [
     PersistenceModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService<EnvironmentVariables>) => ({
+        secret: configService.get('JWT_ACCESS_TOKEN_SECRET'),
         signOptions: { expiresIn: '1h' },
       }),
-      inject: [ConfigService],
     }),
   ],
   providers: [CityService, AuthService, ZipCodeService, EncryptionService],
