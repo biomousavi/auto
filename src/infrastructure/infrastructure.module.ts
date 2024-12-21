ad0329cd92ffac04ffd2f86bde38649b7a56a5a3
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-21 15:12:05 +0330

    feat: add infrastructure services

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,4 +1,26 @@
+// infrastructure/infrastructure.module.ts
 import { Module } from '@nestjs/common';
+import { PersistenceModule } from '../persistence/persistence.module';
+import { CityService } from './services/city.service';
+import { AuthService } from './services/auth.service';
+import { ZipCodeService } from './services/zip-code.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EncryptionService } from './services/encryption.service';
 
-@Module({})
+@Module({
+  imports: [
+    PersistenceModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [CityService, AuthService, ZipCodeService, EncryptionService],
+  exports: [CityService, AuthService, ZipCodeService, EncryptionService],
+})
 export class InfrastructureModule {}
