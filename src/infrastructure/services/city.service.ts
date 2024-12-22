6a7ab2e12cf22622b2480048b22674403c4f3022
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-12-22 23:09:16 +0330

    feat: add get user requests

--- src/infrastructure/services/city.service.ts
+++ src/infrastructure/services/city.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CityRepository } from '../../persistence/repositories/city.repository';
 import { CityRequestDto } from 'src/api/dtos/city-request.dto';
+import { GetRequestsQuery } from 'src/api/queries/get-requests.query';
 
 @Injectable()
 export class CityService {
@@ -15,5 +16,17 @@ export class CityService {
     });
 
     return city;
+  }
+
+  async getCityRequests(query: GetRequestsQuery) {
+    const requests = await this.cityRepository.findAll({
+      where: { user: { id: query.userId } },
+      relations: ['places', 'user'],
+      order: { created_on: 'DESC' },
+      take: query.limit,
+      skip: (query.page - 1) * query.limit,
+    });
+
+    return requests;
   }
 }
